b731700c1cbb3c11e36b4cd468fef410d548184a
lucianocanovas <canovaslucho11@gmail.com>
2025-09-26 01:39:10 -0300

    Added jeep assets

--- src/map_manager.py
+++ src/map_manager.py
@@ -1,10 +1,1 @@
-import os
 
-asset_dir = '../assets/vehicles/jeep'
-
-for filename in os.listdir(asset_dir):
-    if filename.startswith('red_'):
-        continue  # Skip files already renamed
-    old_path = os.path.join(asset_dir, filename)
-    new_path = os.path.join(asset_dir, f'red_{filename}')
-    os.rename(old_path, new_path)
