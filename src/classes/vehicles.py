e3a96ae2b68025510a93fe6936cb7cf677042790
mateo23peralta <mateo23.peralta@gmail.com>
2025-09-27 18:32:23 -0300

    Base de vehiculo ya casi terminada. Añadi clase de Minas y no lo toque.

--- src/classes/vehicles.py
+++ src/classes/vehicles.py
@@ -1,35 +1,60 @@
-class vehicle:
-    def __init__(self, capacity):
-        self.position = [None, None]
-        self.capacity = capacity
+import pygame.image
 
-    def move(self):
-        pass
+import items, map, mines
+
+class Vehicle:
+    def __init__(self, capacity,x,y,image_path):
+        self.position = [x,y]
+        self.capacity = capacity
+        self.image = pygame.image.load(image_path)
+
+    def move(self,direction):
+
+        x,y = self.position
+
+        if direction == "left" :
+            self.position = [x-1,y]
+        elif  direction == "right" :
+            self.position = [x+1,y]
+        elif  direction == "up" :
+            self.position = [x,y+1]
+        elif  direction == "down" :
+            self.position = [x,y-1]
+
+        return self.position
 
     def download(self):
         pass
 
-    def findShortestPath(self):
+    def find_shortest_path(self):
         pass
 
-class jeep(vehicle):
-    def __init__(self):
-        super().__init__(2)
+    def check_full(self):
+        if self.capacity == 0 :
+            return True
+        return False
 
-class truck(vehicle):
-    def __init__(self):
-        super().__init__(3)
+class Jeep(Vehicle):
+    def __init__(self, x, y):
+        super().__init__(2, x, y,)
 
-class car(vehicle):
-    def __init__(self):
-        super().__init__(1)
 
-class motorcicle(vehicle):
-    def __init__(self):
-        super().__init__(1)
+class Truck(Vehicle):
+    def __init__(self, x, y):
+        super().__init__(3, x, y,)
 
-    def checkPerson():
-        pass
+
+class Car(Vehicle):
+    def __init__(self, x, y):
+        super().__init__(1, x, y)
+
+
+class Motorcycle(Vehicle):
+    def __init__(self, x, y):
+        super().__init__(1, x, y,)
+
+    def check_person(self,items):
+         pass
 
     def kamikaze():
         pass
